951d77f27d1fd16062e7edd1c5bcc682e144e831
Guney Sogut <guneysogut.10@gmail.com>
2022-01-23 16:47:42 +0300

    new workout is added

--- VATCalculator.java
+++ VATCalculator.java
@@ -6,16 +6,16 @@ public class VATCalculator {
         // Price calculating by including VAT
 
         Scanner input = new Scanner(System.in);
-
+        
         double priceWithVAT = 0;
         double price=-1;
-
+        
         while(price<0)
         {
             System.out.print("Please enter the price : ");
             price = input.nextDouble();
         }
-
+        
         double vat = price<1000? 0.18 : 0.8;
         priceWithVAT = price + price * vat;
         System.out.println("Price with VAT : " + priceWithVAT);
